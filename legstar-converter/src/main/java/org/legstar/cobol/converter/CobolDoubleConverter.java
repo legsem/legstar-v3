997ec8412843b32d8b8e9a386ba46c3ae43c4932
fmoussallam <fmoussallam@talend.com>
2026-05-01 11:40:21 +0200

    Java to COBOL conversion for double type

--- legstar-converter/src/main/java/org/legstar/cobol/converter/CobolDoubleConverter.java
+++ legstar-converter/src/main/java/org/legstar/cobol/converter/CobolDoubleConverter.java
@@ -11,6 +11,13 @@ import org.legstar.cobol.utils.BytesLenUtils;
  */
 public class CobolDoubleConverter {
 
+	private static final long SIGN_BIT_MASK = 0x8000000000000000L; // bit 63 is the sign
+	private static final long EXP_IEEE_MASK = 0x7ff0000000000000L; // bits 62 to 52 is the encoded binary exponent
+	private static final long MAN_IEEE_MASK = 0x000fffffffffffffL; // bits 51 to 0 is the encoded mantissa
+	private static final long EXP_COMP_2_MASK = 0x7f00000000000000L; // bits 62 to 56 is the encoded hexadecimal
+																		// exponent
+	private static final long MAN_COMP_2_MASK = 0x00ffffffffffffffL; // bits 56 to 0 is the mantissa
+
 	/**
 	 * Build a COBOL double converter
 	 */
@@ -64,18 +71,9 @@ public class CobolDoubleConverter {
 			ByteBuffer bb = ByteBuffer.wrap(buffer);
 			long hostLongBits = bb.getLong();
 
-			/* First bit left (bit 63) is the sign: 0 = positive, 1 = negative */
-			int sign = (int) ((hostLongBits & 0x8000000000000000L) >>> 63);
-
-			/*
-			 * Bits 62-56 (7 bits) represents the exponent offset by 64, this number is
-			 * called excess so you get the exponent as E= excess - 64
-			 */
-			int excess = (int) ((hostLongBits & 0x7f00000000000000L) >>> 56);
-			int exponent = excess == 0 ? 0 : (excess - 64);
-
-			/* Bits 55-0 (56 bits) represents the mantissa. */
-			long mantissa = hostLongBits & 0x00ffffffffffffffL;
+			long sign = sign(hostLongBits);
+			long exponent = comp_2Exponent(hostLongBits);
+			long mantissa = comp_2Mantissa(hostLongBits);
 
 			/* Java Doubles are in IEEE 754 floating-point bit layout. */
 			/*
@@ -104,8 +102,8 @@ public class CobolDoubleConverter {
 			if (exponent != 0 || mantissa != 0) {
 				/* Get rid of the leading 1 which needs to be implicit */
 				long javaLongBits = mantissa & 0x000fffffffffffffL;
-				javaLongBits = javaLongBits | ((long) (exponent + 1023) << 52);
-				javaLongBits = javaLongBits | ((long) sign << 63);
+				javaLongBits = javaLongBits | ((exponent + 1023) << 52);
+				javaLongBits = javaLongBits | (sign << 63);
 				result = Double.longBitsToDouble(javaLongBits);
 			}
 
@@ -113,6 +111,108 @@ public class CobolDoubleConverter {
 		} catch (IOException e) {
 			throw new CobolBeanConverterException(e);
 		}
+	}
+
+	/**
+	 * Convert a java double to a COBOL long floating point (COMP-2)
+	 * <p>
+	 * https://en.wikipedia.org/wiki/Double-precision_floating-point_format
+	 * <p>
+	 * https://en.wikipedia.org/wiki/IBM_hexadecimal_floating-point
+	 * <p>
+	 * Java double is in IEEE 724 format where the exponent in a binary exponent.
+	 * The layout is like this:
+	 * <pre>
+	 * 0100000001100000101000000000000000000000000000000000000000000000
+	 * -                                                                 sign
+	 *  -----------                                                      exponent
+	 *             ----------------------------------------------------  significand
+	 * </pre>
+	 * 
+	 * <p>
+	 * Otherwise a difficulty is to convert the java binary exponent into a COMP-2
+	 * hexadecimal component (An hexadecimal digit is 4 bits).
+	 * <p>
+	 * In the normal case, there is an implicit initial 1 bit for the java fraction.
+	 * This needs to be accounted for for both the exponent and the mantissa.
+	 * <p>
+	 * The subnormal case is not supported.
+	 * 
+	 * @param value the java double
+	 * @return a COBOL long floating point
+	 */
+	public byte[] toCobol(double value) {
+
+		if (Double.isNaN(value) || Double.isInfinite(value)) {
+			throw new CobolBeanConverterException("Unsupported double " + value);
+		}
+		if (value == 0.0d || value == -0.0d) {
+			return new byte[8];
+		}
+
+		long bitsIeee = Double.doubleToLongBits(value);
+		long sign = sign(bitsIeee);
+		long expIeee = ((bitsIeee & EXP_IEEE_MASK) >> 52) - 1023; // remove bias
+		long manIeee = bitsIeee & MAN_IEEE_MASK;
+
+		if (expIeee == 0 && manIeee != 0) {
+			throw new CobolBeanConverterException("Subnormal doubles are not supported");
+		}
+		/* IEEE exponent + implied first bit 1 may not be a multiple of 4 */
+		long rest = (expIeee + 1) % 4; 
+		/* Calculate shift to apply on mantissa to accommodate a hex component */
+		long shift = rest <= 0 ? -1 * rest : 4 - rest;
+		/* Calculate the biased hex exponent for COMP-2 */
+		long expComp_2 = (((expIeee + 1 + shift) / 4)) + 64; 
+		if (expComp_2 < 0 || expComp_2 > 127) {
+			throw new CobolBeanConverterException(
+					"Hexadecimal biased exponent " + expComp_2 + " outside supported range (0-127)");
+		}
+		/* COMP-2 mantissa must include implicit first bit 1 */
+		long manComp_2 = (1l << 52) | manIeee;
+		/* shift mantissa to account for hex exponent */
+		manComp_2 = manComp_2 >> shift; 
+		/* Compensate for extra 3 bits introduced by adding implicit first bit */
+		manComp_2 = manComp_2 << 3; 
+
+		long bitsComp_2 = sign << 63;
+		bitsComp_2 |= ((expComp_2 << 56) & EXP_COMP_2_MASK);
+		bitsComp_2 |= manComp_2 & MAN_COMP_2_MASK;
+		ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+		buffer.putLong(bitsComp_2);
+		return buffer.array();
+	}
+
+	/**
+	 * Sign is the leftmost bit in IEEE and COMP-2
+	 * 
+	 * @param bits the COMP-2 or IEEE 64 bits
+	 * @return 0 if positive, 1 if negative
+	 */
+	public static long sign(long bits) {
+		return (bits & SIGN_BIT_MASK) >>> 63;
+	}
+
+	/**
+	 * For COMP-2, bits 62-56 (7 bits) represents the exponent offset by 64, this
+	 * number is called excess so you get the exponent as E= excess - 64
+	 * 
+	 * @param bits the COMP-2 64 bits
+	 * @return the hexadecimal exponent
+	 */
+	public static long comp_2Exponent(long bits) {
+		long excess = (bits & EXP_COMP_2_MASK) >>> 56;
+		return excess == 0 ? 0 : (excess - 64);
+	}
+
+	/**
+	 * For COMP-2 the mantissa is the trailing 56 bits.
+	 * 
+	 * @param bits the COMP-2 64 bits
+	 * @return the mantissa
+	 */
+	public static long comp_2Mantissa(long bits) {
+		return bits & MAN_COMP_2_MASK;
 	}
 
 }
--- legstar-converter/src/test/java/org/legstar/cobol/converter/CobolDoubleConverterTest.java
+++ legstar-converter/src/test/java/org/legstar/cobol/converter/CobolDoubleConverterTest.java
@@ -3,6 +3,8 @@ package org.legstar.cobol.converter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.nio.ByteBuffer;
+
 import org.junit.jupiter.api.Test;
 
 public class CobolDoubleConverterTest extends CobolConverterTestBase {
@@ -19,27 +21,89 @@ public class CobolDoubleConverterTest extends CobolConverterTestBase {
 		}
 	}
 
-    @Test
-    public void testFromHostDouble() {
-        assertEquals(0.0d, fromHost("0000000000000000"));
-        assertEquals(1.0d, fromHost("4110000000000000"));
-        assertEquals(345006.56779999996d, fromHost("45543ae915b573e8"));
-        assertEquals(7.982006699999995E-14d, fromHost("361677a4590fab68"));
-        assertEquals(3.40282347E38d, fromHost("60ffffff048ff9e8"));
-        assertEquals(-5.670078E-14d, fromHost("b5ff5b8c70649ed0"));
-        assertEquals(0.5160421914381664d, fromHost("40841b574f9545cd"));
-        assertEquals(0.3767408804881589d, fromHost("4060721720c34c90"));
-        assertEquals(0.5748594107197613d, fromHost("409329fc80d6d646"));
-        assertEquals(0.6001057816958458d, fromHost("4099a0885286303d"));
-        assertEquals(0.7850369597199326d, fromHost("40c8f82ea425fc38"));
-        assertEquals(0.2998016949252067d, fromHost("404cbfcdcafd37c0"));
-        assertEquals(0.7788391667528523d, fromHost("40c76200ee0c21d0"));
-        assertEquals(0.8158852356638823d, fromHost("40d0dddad4773358"));
-        assertEquals(0.4364272923152456d, fromHost("406fb9b2f3936868"));
-        assertEquals(0.6062248297052384d, fromHost("409b318ce99b6f60"));
-    }
+	@Test
+	public void testFromHostDouble() {
+		assertEquals(0.0d, fromHost("0000000000000000"));
+		assertEquals(1.0d, fromHost("4110000000000000"));
+		assertEquals(345006.56779999996d, fromHost("45543ae915b573e8"));
+		assertEquals(7.982006699999995E-14d, fromHost("361677a4590fab68"));
+		assertEquals(3.40282347E38d, fromHost("60ffffff048ff9e8"));
+		assertEquals(-5.670078E-14d, fromHost("b5ff5b8c70649ed0"));
+		assertEquals(0.5160421914381664d, fromHost("40841b574f9545cd"));
+		assertEquals(0.3767408804881589d, fromHost("4060721720c34c90"));
+		assertEquals(0.5748594107197613d, fromHost("409329fc80d6d646"));
+		assertEquals(0.6001057816958458d, fromHost("4099a0885286303d"));
+		assertEquals(0.7850369597199326d, fromHost("40c8f82ea425fc38"));
+		assertEquals(0.2998016949252067d, fromHost("404cbfcdcafd37c0"));
+		assertEquals(0.7788391667528523d, fromHost("40c76200ee0c21d0"));
+		assertEquals(0.8158852356638823d, fromHost("40d0dddad4773358"));
+		assertEquals(0.4364272923152456d, fromHost("406fb9b2f3936868"));
+		assertEquals(0.6062248297052384d, fromHost("409b318ce99b6f60"));
+	}
+
+	@Test
+	public void testToHostDouble() {
+		assertEquals(-118.625d, toHost(-118.625));
+		assertEquals(128d, toHost(128));
+		assertEquals(-128d, toHost(-128));
+		assertEquals(0d, toHost(0));
+		assertEquals(0d, toHost(-0));
+		assertEquals(1.0d, toHost(1.0));
+		assertEquals(0.1d, toHost(0.1), 0.0000000000001);
+		assertEquals(5.23E-5, toHost(5.23E-5), 0.00000000000001E-5);
+		assertEquals(-5.670078E-14, toHost(-5.670078E-14));
+		assertEquals(3.40282347E38, toHost(3.40282347E38), 0.000000000000001E38);
+		assertEquals(7.982006699999995E-14, toHost(7.982006699999995E-14), 0.0000000000001E-14);
+	}
+
+	@Test
+	public void testToHostDoubleErrors() {
+		toHostError(Double.NaN, "Unsupported double NaN");
+		toHostError(Double.NEGATIVE_INFINITY, "Unsupported double -Infinity");
+		toHostError(Double.POSITIVE_INFINITY, "Unsupported double Infinity");
+		toHostError(Double.MAX_VALUE, "Hexadecimal biased exponent 320 outside supported range (0-127)");
+		toHostError(Double.MIN_NORMAL, "Hexadecimal biased exponent -191 outside supported range (0-127)");
+		toHostError(Double.MIN_VALUE, "Hexadecimal biased exponent -191 outside supported range (0-127)");
+		toHostError(1E-99, "Hexadecimal biased exponent -18 outside supported range (0-127)");
+	}
 
 	private Double fromHost(String payload) {
 		return converter.toDouble(inputStreamFrom(payload));
+	}
+
+	private double toHost(double value) {
+		return fromComp_2(converter.toCobol(value));
+	}
+	
+	private void toHostError(double value, String expectedMsg) {
+		try {
+			toHost(value);
+			fail();
+		} catch (Exception e) {
+			assertEquals(expectedMsg, e.getMessage());
+		}
+	}
+
+	/**
+	 * Evaluate the actual value of a COMP-2 numeric.
+	 * 
+	 * @param comp_2 the COMP-2
+	 * @return the numeric value of the COMP-2 in power of 10
+	 */
+	private double fromComp_2(byte[] comp_2) {
+		ByteBuffer bb = ByteBuffer.wrap(comp_2);
+		long bitsComp_2 = bb.getLong();
+		long sign = CobolDoubleConverter.sign(bitsComp_2);
+		long exponent = CobolDoubleConverter.comp_2Exponent(bitsComp_2);
+		long mantissa = CobolDoubleConverter.comp_2Mantissa(bitsComp_2);
+		double fraction = 0d;
+		int i = 8;
+		for (int p = -1; p > -14; p--) {
+			long left = mantissa << i;
+			long right = left >>> 60;
+			fraction += right * Math.pow(16, p);
+			i = i + 4;
+		}
+		return Math.pow(-1, sign) * (fraction) * Math.pow(16, exponent);
 	}
 }
